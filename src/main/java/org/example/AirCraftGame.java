b91033ff373630fb7df2549d8cbbddf34140ec96
pauline12ish34 <paulineishimwe12@gmail.com>
2025-05-04 23:21:01 +0200

    prompted to enter player name

--- src/main/java/org/example/AirCraftGame.java
+++ src/main/java/org/example/AirCraftGame.java
@@ -3,12 +3,13 @@ package org.example;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
-import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Iterator;
 import javax.sound.sampled.*;
 import java.io.File;
 import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class AirCraftGame extends JPanel implements ActionListener, KeyListener {
     private Timer timer;
@@ -20,28 +21,24 @@ public class AirCraftGame extends JPanel implements ActionListener, KeyListener 
     private boolean movingRight = true;
     private int score = 0;
     private int timeElapsed = 0;
-    private Image aircraftImage, gunImage, explosionImage, fireImage;
-
+    private Image aircraftImage, gunImage, fireImage, backgroundImage;
     private Clip shootClip, gameOverClip;
-//    private DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
-
+    public boolean gameStarted = false;
+    public String playerName = "";
 
     public AirCraftGame() {
-        timer = new Timer(50, this);
-        timer.start();
-        addKeyListener(this);
         setFocusable(true);
         setFocusTraversalKeysEnabled(false);
+        addKeyListener(this);
 
         aircraftImage = new ImageIcon("C:\\Users\\user\\Documents\\Java Ass\\JavaGameAirspace\\Aircraft.png").getImage();
         gunImage = new ImageIcon("C:\\Users\\user\\Documents\\Java Ass\\JavaGameAirspace\\140mm.png").getImage();
-
-//        explosionImage = new ImageIcon("C:\\Users\\user\\Documents\\Java Ass\\JavaGameAirspace\\explosion.png").getImage();
         fireImage = new ImageIcon("C:\\Users\\user\\Documents\\Java Ass\\JavaGameAirspace\\explfire.png").getImage();
+        backgroundImage = new ImageIcon("C:\\Users\\user\\Documents\\Java Ass\\JavaGameAirspace\\bg.jpg").getImage();
 
         loadSounds();
 
-        new Timer(1000, e -> shootBullet()).start();
+        timer = new Timer(50, this);
         new Timer(1000, e -> timeElapsed++).start();
     }
 
@@ -56,7 +53,6 @@ public class AirCraftGame extends JPanel implements ActionListener, KeyListener 
             AudioInputStream gameOverAudio = AudioSystem.getAudioInputStream(gameOverSoundFile);
             gameOverClip = AudioSystem.getClip();
             gameOverClip.open(gameOverAudio);
-
         } catch (UnsupportedAudioFileException | IOException | LineUnavailableException e) {
             e.printStackTrace();
         }
@@ -70,8 +66,8 @@ public class AirCraftGame extends JPanel implements ActionListener, KeyListener 
     }
 
     public void shootBullet() {
-        if (!gameOver) {
-            bullets.add(new int[]{gunX + 20, gunY});
+        if (!gameOver && gameStarted) {
+            bullets.add(new int[]{gunX + 10, gunY});
             playSound(shootClip);
         }
     }
@@ -79,36 +75,33 @@ public class AirCraftGame extends JPanel implements ActionListener, KeyListener 
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
 
-        Image backgroundImage = new ImageIcon("C:\\Users\\user\\Documents\\Java Ass\\JavaGameAirspace\\bg.jpg").getImage();
+        if (!gameStarted) {
+            g.setColor(Color.BLACK);
+            g.fillRect(0, 0, getWidth(), getHeight());
+            g.setColor(Color.WHITE);
+            g.setFont(new Font("Arial", Font.BOLD, 28));
+            g.drawString("Welcome to plane Escape Game", 70, 200);
+            g.setFont(new Font("Arial", Font.PLAIN, 20));
+            g.drawString("Enter your name above and click Start to begin.", 100, 240);
+            return;
+        }
+
         if (backgroundImage != null) {
             g.drawImage(backgroundImage, 0, 0, getWidth(), getHeight(), this);
         }
 
-        if (exploded) {
-//            if (explosionImage != null) {
-//                g.drawImage(explosionImage, aircraftX, aircraftY, 100, 80, this);
-//            }
-            if (fireImage != null) {
-                g.drawImage(fireImage, aircraftX + 10, aircraftY + 10, 300, 300, this);
-            }
-        } else {
-            if (aircraftImage != null) {
-                g.drawImage(aircraftImage, aircraftX, aircraftY, 100, 80, this);
-            } else {
-                g.setColor(Color.WHITE);
-                g.fillRect(aircraftX, aircraftY, 60, 30);
-            }
+        if (exploded && fireImage != null) {
+            g.drawImage(fireImage, aircraftX + 10, aircraftY + 10, 300, 300, this);
+        } else if (aircraftImage != null) {
+            g.drawImage(aircraftImage, aircraftX, aircraftY, 100, 80, this);
         }
 
         if (gunImage != null) {
             g.drawImage(gunImage, gunX, gunY, 100, 80, this);
-        } else {
-            g.setColor(Color.BLUE);
-            g.fillRect(gunX, gunY, 40, 20);
         }
 
-        if (gunX < aircraftX) gunX += 2;
-        if (gunX > aircraftX) gunX -= 2;
+        if (gunX < aircraftX) gunX += 7;
+        if (gunX > aircraftX) gunX -= 7;
 
         g.setColor(Color.RED);
         Iterator<int[]> iterator = bullets.iterator();
@@ -133,7 +126,11 @@ public class AirCraftGame extends JPanel implements ActionListener, KeyListener 
         g.setColor(Color.WHITE);
         g.drawString("Time: " + timeElapsed + "s", 20, 20);
         g.drawString("Score: " + score, 20, 40);
-//        g.drawString("date",+ datee, 20, 60);
+        g.drawString("Player: " + playerName, 20, 60);
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        String currentDateTime = LocalDateTime.now().format(formatter);
+        g.drawString("Date: " + currentDateTime, 400, 20);
 
         if (gameOver) {
             g.setColor(Color.RED);
@@ -168,7 +165,7 @@ public class AirCraftGame extends JPanel implements ActionListener, KeyListener 
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        if (!gameOver) {
+        if (!gameOver && gameStarted) {
             if (movingRight) {
                 aircraftX += 5;
                 if (aircraftX > getWidth() - 100) {
@@ -187,7 +184,11 @@ public class AirCraftGame extends JPanel implements ActionListener, KeyListener 
     }
 
     @Override
-    public void keyPressed(KeyEvent e) {}
+    public void keyPressed(KeyEvent e) {
+        if (e.getKeyCode() == KeyEvent.VK_J && !gameOver && gameStarted) {
+            shootBullet();
+        }
+    }
 
     @Override
     public void keyReleased(KeyEvent e) {}
@@ -198,7 +199,39 @@ public class AirCraftGame extends JPanel implements ActionListener, KeyListener 
     public static void main(String[] args) {
         JFrame frame = new JFrame("Aircraft Escape Game");
         AirCraftGame gamePanel = new AirCraftGame();
-        frame.add(gamePanel);
+
+        JPanel welcomePanel = new JPanel();
+        welcomePanel.setLayout(new FlowLayout());
+        JTextField nameField = new JTextField(20);
+        JButton startButton = new JButton("Start Game");
+        startButton.setBackground(Color.blue);
+        startButton.setForeground(Color.WHITE);
+        startButton.setFont(new Font("Arial", Font.BOLD, 15));
+
+        welcomePanel.add(new JLabel("Enter Player Name: "));
+        welcomePanel.add(nameField);
+        welcomePanel.add(startButton);
+
+        frame.setLayout(new BorderLayout());
+        frame.add(welcomePanel, BorderLayout.NORTH);
+        frame.add(gamePanel, BorderLayout.CENTER);
+
+        startButton.addActionListener(e -> {
+            gamePanel.playerName = nameField.getText().trim();
+            if (gamePanel.playerName.isEmpty()) {
+                JOptionPane.showMessageDialog(frame, "Please enter your name.");
+                return;
+            }
+            gamePanel.gameStarted = true;
+            frame.remove(welcomePanel);
+            frame.revalidate();
+            frame.repaint();
+
+            gamePanel.timer.start();
+            gamePanel.requestFocusInWindow(); // Allow key input
+            gamePanel.repaint();
+        });
+
         frame.setSize(600, 600);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
